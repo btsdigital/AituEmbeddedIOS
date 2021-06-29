d3de9343a5bb41740ef9fd88d70fc51775274db5
Artem Mylnikov <artem.mylnikov@btsdigital.kz>
2021-06-30 00:24:14 +0700

    Add sample

--- Sample/AppDelegate.swift
+++ Sample/AppDelegate.swift
@@ -1,4 +1,6 @@
 import UIKit
+import WebKit
+import AituEmbedded
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,9 +9,38 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = UIViewController()
+        window?.rootViewController = AituViewController()
         window?.rootViewController?.view.backgroundColor = .green
         window?.makeKeyAndVisible()
         return true
+    }
+}
+
+final class AituViewController: UIViewController, AituWebBridgeDelegate {
+    enum DemoError: Error {
+        case something
+    }
+    private let webView = WKWebView()
+    private var bridge: AituWebBridge?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bridge = AituWebBridge(webView: webView)
+        bridge?.delegate = self
+        bridge?.start()
+
+        webView.frame = view.bounds
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(webView)
+
+        let requst = URLRequest(url: URL(string: "https://astanajs.kz/test-kundelik")!)
+        webView.load(requst)
+    }
+
+    func getToken(completed: @escaping (Result<String, Error>) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+//            completed(.failure(DemoError.something))
+            completed(.success("kz123"))
+        })
     }
 }
